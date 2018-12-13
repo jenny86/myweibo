37a702bbcf536f1aab77805f8f8ec16c7913a92b
xiaochuan8818 <wangyunchuan3410@163.com>
2018-12-13 12:06:56 +0800

    master

--- src/base/promise.js
+++ src/base/promise.js
@@ -15,7 +15,14 @@ export default
   axios({
     method,
     url,
-    data
+    data,
+    transformRequest: [function (data) {
+      let ret = ''
+      for (let it in data) {
+        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+      }
+      return ret
+    }]
   })
     .then((res)=>{
 
